4388fa6eed94957c641af84d6fb055e1d2e4ead9
Borys Szygenda <szygenda.borys@gmail.com>
2021-01-31 22:15:20 +0100

    working deployment on npm registry

---
+++ dev/index.ts
@@ -0,0 +1,12 @@
+import Duration from './duration/Duration';
+import DateFilter from './period/filters/DateFilter';
+import PeriodFilter from './period/filters/PeriodFilter';
+import Period from './period/Period';
+import PeriodConstants from './period/PeriodConstants';
+import PeriodTimeChanger from './period/PeriodTimeChanger';
+import DateSorter from './period/sorters/DateSorter';
+import PeriodDateSorter from './period/sorters/PeriodDateSorter';
+
+export = {
+    Period, Duration, DateFilter, PeriodFilter, PeriodConstants, PeriodTimeChanger, DateSorter, PeriodDateSorter
+}
